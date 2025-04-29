e3be03614fd1365e7827e5fb0fd17dd770421a23
fungsilent <funglee162@gmail.com>
2025-04-29 10:57:14 +0800

    added login page

--- src/app/layout.js
+++ src/app/layout.js
@@ -37,7 +37,7 @@ export default function Layout({ children }) {
         <SidebarProvider>
           <AppSidebar />
           <main>
-            <SidebarTrigger />
+            {/* <SidebarTrigger /> */}
             {children}
           </main>
         </SidebarProvider>
